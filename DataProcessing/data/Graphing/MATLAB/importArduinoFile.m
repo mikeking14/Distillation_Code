ee38fd726c3649f693347bf2a55f8128e8710d5b
mikeking14 <mikeking14@gmail.com>
2020-06-30 13:00:02 -0700

    Update to Data Importation Format

--- DataProcessing/data/Graphing/MATLAB/importArduinoFile.m
+++ DataProcessing/data/Graphing/MATLAB/importArduinoFile.m
@@ -1,4 +1,4 @@
-function arduino = importArduinoFile(filename, dataLines)
+function arduino = importArduinoFile_old(filename, dataLines)
 %IMPORTFILE Import data from a text file
 %  Arduino = IMPORTARDUINOFILE(FILENAME) reads data from text
 %  file FILENAME for the default selection.  Returns the data as a table.
@@ -21,26 +21,27 @@ if nargin < 2
 end
 
 %% Setup the Import Options and import the data
-opts = delimitedTextImportOptions("NumVariables", 33);
+opts = delimitedTextImportOptions("NumVariables", 44);
 
 % Specify range and delimiter
-opts.DataLines = dataLines;
-opts.Delimiter = ["\t",">" ","]; 
+opts.DataLines = [2, Inf];
+opts.Delimiter = ["\t", ">"];
 
 % Specify column names and types
-opts.VariableNames = ["Time", "Var2", "SetPoint", "Var4", "CurrentSetPosition", "Var6", "PID_Error", "Var8", "PID", "Var10", "P", "Var12", "I", "Var14", "D", "Var16", "Wash_Temp", "Var18", "Outlet_Temp", "Var20", "Mass", "Var22", "Mass_Delta", "Var24", "Frequency", "Var26", "Tower_Temp", "Var28", "Set_Temp", "Var30", "Set_Temp_Counter", "Var32", "CheckPoint"];
-opts.SelectedVariableNames = ["Time", "SetPoint", "CurrentSetPosition", "PID_Error", "PID", "P", "I", "D", "Wash_Temp", "Outlet_Temp", "Mass", "Mass_Delta", "Frequency", "Tower_Temp", "Set_Temp", "Set_Temp_Counter", "CheckPoint"];
-opts.VariableTypes = ["datetime", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double"];
+opts.VariableNames = ["Time", "Var2", "Var3", "SetPosition", "Var5", "CurrentPossition", "Var7", "PID_Error", "Var9", "PID", "Var11", "P", "Var13", "I", "Var15", "D", "Var17", "Room_Temp", "Var19", "Wash_Temp", "Var21", "Outlet_Temp", "Var23", "Tower_Temp", "Var25", "Mass", "Var27", "Mass_Delta", "Var29", "Frequency", "Var31", "Set_Temp", "Var33", "Set_Temp_Counter", "Var35", "CheckPoint", "Var37", "State", "Var39", "kP", "Var41", "kI", "Var43", "kD"];
+opts.SelectedVariableNames = ["Time", "SetPosition", "CurrentPossition", "PID_Error", "PID", "P", "I", "D", "Room_Temp", "Wash_Temp", "Outlet_Temp", "Tower_Temp", "Mass", "Mass_Delta", "Frequency", "Set_Temp", "Set_Temp_Counter", "CheckPoint", "State", "kP", "kI", "kD"];
+opts.VariableTypes = ["datetime", "string", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double", "string", "double"];
 
 % Specify file level properties
 opts.ImportErrorRule = "omitrow";
 opts.MissingRule = "omitrow";
 opts.ExtraColumnsRule = "ignore";
 opts.EmptyLineRule = "read";
+opts.ConsecutiveDelimitersRule = "join";
 
 % Specify variable properties
-opts = setvaropts(opts, ["Var2", "Var4", "Var6", "Var8", "Var10", "Var12", "Var14", "Var16", "Var18", "Var20", "Var22", "Var24", "Var26", "Var28", "Var30", "Var32"], "WhitespaceRule", "preserve");
-opts = setvaropts(opts, ["Var2", "Var4", "Var6", "Var8", "Var10", "Var12", "Var14", "Var16", "Var18", "Var20", "Var22", "Var24", "Var26", "Var28", "Var30", "Var32"], "EmptyFieldRule", "auto");
+opts = setvaropts(opts, ["Var2", "Var3", "Var5", "Var7", "Var9", "Var11", "Var13", "Var15", "Var17", "Var19", "Var21", "Var23", "Var25", "Var27", "Var29", "Var31", "Var33", "Var35", "Var37", "Var39", "Var41", "Var43"], "WhitespaceRule", "preserve");
+opts = setvaropts(opts, ["Var2", "Var3", "Var5", "Var7", "Var9", "Var11", "Var13", "Var15", "Var17", "Var19", "Var21", "Var23", "Var25", "Var27", "Var29", "Var31", "Var33", "Var35", "Var37", "Var39", "Var41", "Var43"], "EmptyFieldRule", "auto");
 opts = setvaropts(opts, "Time", "InputFormat", "HH:mm:ss.SSS");
 
 % Import the data
