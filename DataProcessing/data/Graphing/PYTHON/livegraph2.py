111931414a7d5444638cda25a74f2f8911f6da58
Jacob Vanzella <JacobVanzella@users.noreply.github.com>
2021-01-24 19:50:15 -0800

    Deleting junk
    Removed a bunch of system dependent junk.

--- DataProcessing/data/Graphing/PYTHON/livegraph2.py
+++
@@ -1,9 +0,0 @@
-import pyrealtime as prt
-
-serial_port = '/dev/cu.usbmodem14201';
-baud_rate = 115200;
-
-
-serial_layer = prt.SerialReadLayer(serial_port = serial_port, baud_rate=baud_rate)
-prt.TimePlotLayer(serial_layer, window_size=100, ylim=(0, 100))
-prt.LayerManager.session().run()
